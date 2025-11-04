2a13358a70fc0a1b9b0ff1a836262aaae97b55a4
strauss <strauss@users.noreply.github.com>
2025-11-04 18:49:37 +0100

    feat(client) Moved function for combining two ints into one long to "global" for using it from outside.

--- mazegame-client-ktor/src/main/kotlin/de/dreamcube/mazegame/client/maze/Bait.kt
+++ mazegame-client-ktor/src/main/kotlin/de/dreamcube/mazegame/client/maze/Bait.kt
@@ -23,6 +23,12 @@ data class Bait(val type: BaitType, val x: Int, val y: Int) {
         /**
          * Internal function for turning the [x] and [y] coordinates into an internal [id].
          */
-        internal fun toId(x: Int, y: Int): Long = x.toLong().shl(32) or (y.toLong() and 0xFFFF_FFFFL)
+        internal fun toId(x: Int, y: Int): Long = combineIntsToLong(x, y)
     }
 }
+
+/**
+ * Function for turning the [x] and [y] coordinates into a long number. [x] is placed in the higher order bits and [y]
+ * is placed in the lower order bits. Signs are truncated and therefore ignored. The bit values are taken "as is".
+ */
+fun combineIntsToLong(x: Int, y: Int): Long = x.toLong().shl(32) or (y.toLong() and 0xFFFF_FFFFL)
